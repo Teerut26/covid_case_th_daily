497dcd761711efd1c7e193be873a29da10f2bd2b
Teerut26 <46746401+Teerut26@users.noreply.github.com>
2020-04-26 20:31:24 +0700

    Update get_json.php

--- get_json.php
+++ get_json.php
@@ -1,11 +1,5 @@
 <?php
-/*$url = file_get_contents("https://covid19.th-stat.com/api/open/today");
-$start_cut = strpos($url,"<span>(เพิ่มขึ้น 15)</span>");
-$page = substr($url,$start_cut);
-$stop_cut = strpos($url,"</span>",0);
-echo $page;*/
 $myjson = file_get_contents('https://covid19.th-stat.com/api/open/today/');
 $json = json_decode($myjson, true);
 $NewConfirmed = $json['NewConfirmed'];
-//$UpdateDate = $json['UpdateDate'];
 ?>
